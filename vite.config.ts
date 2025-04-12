f1503561ab441ae2e7e70b7f205a80b34b123fe7
Artem Mozgovoi <3372745+layonez@users.noreply.github.com>
2025-04-12 23:05:10 +0200

    fix: update base path for GitHub Pages in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/xvat2/', // Set the base path for GitHub Pages
+  base: '/xvat/',
 });
